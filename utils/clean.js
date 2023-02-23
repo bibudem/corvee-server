f68a9690085e45409b19d3bdb8035a11ffa24ebe
Christian Rémillard <christian.remillard@gmail.com>
2023-02-23 10:28:45 -0500

    wip

--- utils/clean.js
+++ utils/clean.js
@@ -1,6 +1,5 @@
 import { fileURLToPath } from 'node:url'
-import { dirname, join } from 'node:path'
-import rimraf from 'rimraf'
+import { rimrafSync } from 'rimraf'
 import pkg from '../package.json' assert {type: 'json'}
 
 const version = pkg.version
@@ -8,7 +7,7 @@ const task = process.argv[2]
 const __dirname = fileURLToPath(new URL(`../${task}/${version}/`, import.meta.url))
 
 if (!['build', 'dev'].includes(task)) {
-  throw new Error(`task parameter must be one of 'build' or 'dev'. Got '${task}'`)
+  throw new Error(`Task parameter must be one of 'build' or 'dev'. Got '${task}'`)
 }
 
-rimraf.sync(`${__dirname}*`)
+rimrafSync(`${__dirname}`)
