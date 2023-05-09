c7eb62b0fc9118045eb521d6142d54bf61f1d6c5
Christian Rémillard <christian.remillard@gmail.com>
2023-05-09 17:06:56 -0400

    fix: Fix wrong import path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk'
 import config from 'config'
 
-import { app } from './app/app.js'
+import { app } from './server/app.js'
 
 app.listen(config.get('server.port.http'), () => {
   console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), config.get('server.port.http'), process.env.NODE_ENV)
