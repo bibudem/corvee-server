06ddc9297fd961b84c64f6028e43040f72039c83
Christian Rémillard <christian.remillard@umontreal.ca>
2026-03-23 11:07:48 -0400

    Correction mineure

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import config from 'config'
 
 import { app } from './server/app.js'
 
-app.listen(config.get('server.port.http'), () => {
-  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), config.get('server.port.http'), process.env.NODE_ENV)
+const PORT = config.get('server.port.http')
+
+app.listen(PORT, () => {
+  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), PORT, process.env.NODE_ENV)
 })
--- server.js
+++ server.js
@@ -19,16 +19,19 @@ const http2Options = {
 const httpServer = http.createServer(app)
 // const httpsServer = https.createServer(credentials, app)
 
-httpServer.listen(config.get('server.port.http'), () => {
-  console.log('%s App is running at https://localhost:%d in %s mode', chalk.green('✓'), config.get('server.port.http'), process.env.NODE_ENV)
+const HTTP_PORT = config.get('server.port.http') || 80
+const HTTPS_PORT = config.get('server.port.https') || 443
+
+httpServer.listen(HTTP_PORT, () => {
+  console.log('%s App is running at https://localhost:%d in %s mode', chalk.green('✓'), HTTP_PORT, process.env.NODE_ENV)
 })
 
 const http2Server = createSecureServer(http2Options, app)
 
-http2Server.listen(config.get('server.port.https'), () => {
-  console.log('%s App is running at https://localhost:%d in %s mode', chalk.green('✓'), config.get('server.port.https'), process.env.NODE_ENV)
+http2Server.listen(HTTPS_PORT, () => {
+  console.log('%s App is running at https://localhost:%d in %s mode', chalk.green('✓'), HTTPS_PORT, process.env.NODE_ENV)
 })
 
-// httpsServer.listen(config.get('server.port.https'), () => {
-//   console.log('%s App is running at https://localhost:%d in %s mode', chalk.green('✓'), config.get('server.port.https'), process.env.NODE_ENV)
+// httpsServer.listen(HTTPS_PORT, () => {
+//   console.log('%s App is running at https://localhost:%d in %s mode', chalk.green('✓'), HTTPS_PORT, process.env.NODE_ENV)
 // })
